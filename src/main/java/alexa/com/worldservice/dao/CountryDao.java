b1643d8a3632aae72c592630ffd9712ebc896b94
AlexaBiloshytska <aleksandranecheporuk@gmail.com>
2019-07-08 22:15:05 +0300

    cleanup all unstructured changes

--- src/main/java/alexa/com/worldservice/dao/CountryDao.java
+++
@@ -1,7 +0,0 @@
-package alexa.com.worldservice.dao;
-
-import alexa.com.worldservice.entity.CountryLanguageStatistics;
-
-public interface CountryDao {
-    CountryLanguageStatistics getStatistics(String name);
-}
