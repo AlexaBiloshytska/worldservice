86d8a60639f7ed009de76a64167e122e613a3b1c
AlexaBiloshytska <aleksandranecheporuk@gmail.com>
2019-07-13 11:59:52 +0300

    set configures

--- src/main/java/com/alexa/worldservice/servlet/CountriesByLanguageServlet.java
+++ src/main/java/com/alexa/worldservice/servlet/CountriesByLanguageServlet.java
@@ -1,14 +1,15 @@
 package com.alexa.worldservice.servlet;
 
 import com.alexa.worldservice.ServiceLocator;
+import com.alexa.worldservice.constant.MimeType;
 import com.alexa.worldservice.service.CountryService;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shelberg.entity.Country;
 import com.shelberg.entity.Views;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.activation.MimeType;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,6 +21,7 @@ import java.util.List;
 @WebServlet(urlPatterns = "/api/v1/language")
 public class CountriesByLanguageServlet extends HttpServlet {
     private final Logger logger = LoggerFactory.getLogger(getClass());
+
     private CountryService countryService = ServiceLocator.get(CountryService.class);
 
     @Override
@@ -27,20 +29,20 @@ public class CountriesByLanguageServlet extends HttpServlet {
         String language = request.getParameter("language");
 
         logger.info("Getting country with language {} ", language);
+        String acceptType = request.getHeader("Accept");
 
-
-        if (MimeType.APPLICATION_XML.getValue().equals(acceptType)) {
+        if (MimeType.APPLICATION_JSON.getValue().equals(acceptType)) {
             List<Country> countries = countryService.getCountriesByLanguage(language);
-
-            String xml = xmlMapper.writeValueAsString(countries);
-            response.getWriter().print(xml);
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(countries);
+            response.getWriter().print(json);
         } else {
             response.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
         }
 
-        logger.info("Finished getting country language statistics in {} ms", startTime - System.currentTimeMillis());
+        logger.info("Finished getting contries  by language  {} ", language);
     }
-    }
+}
 
 
-    }
+
