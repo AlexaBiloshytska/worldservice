118015b8971a4c03504c3644d6a8e457d09e3724
AlexaBiloshytska <aleksandranecheporuk@gmail.com>
2019-07-16 13:38:44 +0300

    Logging added to JdbcCountryDao search country by name;

--- src/main/java/com/alexa/worldservice/dao/jdbc/JdbcCountryDao.java
+++ src/main/java/com/alexa/worldservice/dao/jdbc/JdbcCountryDao.java
@@ -5,6 +5,8 @@ import com.alexa.worldservice.mapper.CountryMapper;
 import com.alexa.worldservice.mapper.LanguageMapper;
 import com.shelberg.entity.Country;
 import com.shelberg.entity.Language;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -15,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcCountryDao implements CountryDao {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private static final CountryMapper COUNTRY_MAPPER = new CountryMapper();
     private static final LanguageMapper COUNTRY_LANGUAGE_MAPPER = new LanguageMapper();
     private static final String GET_LANGUAGE_STATISTICS = "select c.code," +
@@ -39,7 +42,18 @@ public class JdbcCountryDao implements CountryDao {
             "left join city as t on t.id = c.capital " +
             "where lang.language = ?";
 
-    private static final String GET_COUNTRY_BY_NAME="";
+    private static final String GET_COUNTRY_BY_NAME = "select c.code,\n" +
+            "c.name," +
+            "c.continent, " +
+            "c.region, " +
+            "c.surfacearea, " +
+            "c.indepyear, " +
+            "c.headofstate " +
+            "from country as c --left join city as t on (t.id = c.capital) " +
+            "where lower(c.name) like %?% ||lower(%?%)||%? " +
+            "AND c.continent = ? " +
+            "AND c.population >= 1000;";
+
     private DataSource dataSource;
 
     public JdbcCountryDao(DataSource dataSource) {
@@ -49,7 +63,7 @@ public class JdbcCountryDao implements CountryDao {
     public Country getCountry(String name) {
 
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(GET_LANGUAGE_STATISTICS);) {
+             PreparedStatement preparedStatement = connection.prepareStatement(GET_LANGUAGE_STATISTICS)) {
 
             preparedStatement.setString(1, name);
 
@@ -60,7 +74,7 @@ public class JdbcCountryDao implements CountryDao {
 
             List<Language> languages = new ArrayList<>();
             languages.add(COUNTRY_LANGUAGE_MAPPER.mapRow(resultSet));
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 languages.add(COUNTRY_LANGUAGE_MAPPER.mapRow(resultSet));
             }
 
@@ -79,7 +93,7 @@ public class JdbcCountryDao implements CountryDao {
 
             preparedStatement.setString(1, language);
 
-            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 List<Country> countries = new ArrayList<>();
                 while (resultSet.next()) {
                     countries.add(COUNTRY_MAPPER.mapRow(resultSet));
@@ -94,9 +108,34 @@ public class JdbcCountryDao implements CountryDao {
     }
 
     @Override
-    public List<Country> searchByName(String name, int page) {
-        return null;
+    public List<Country> searchByName(String name, String continent, Integer population) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(GET_COUNTRY_BY_NAME)) {
+            if (name != null && !name.isEmpty()) {
+                String queryName = GET_COUNTRY_BY_NAME + "where lower(c.name) like? OR lower(?)OR?";
+            }
+            if (continent != null && !continent.isEmpty()) {
+                String queryContinent = GET_COUNTRY_BY_NAME + "AND c.continent =?";
+            }
+            if (population != null) {
+                String queryPopulation = GET_COUNTRY_BY_NAME + "AND c.population >= 1000";
+            }
+
+            preparedStatement.setString(1, "name");
+            preparedStatement.setString(2, "continent");
+            preparedStatement.setInt(3, population);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+           List <Country >country = COUNTRY_MAPPER.mapRow(resultSet);
+
+            return country;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to execute sql query: " + GET_COUNTRY_BY_NAME, e);
+        }
     }
+
 
 }
 
