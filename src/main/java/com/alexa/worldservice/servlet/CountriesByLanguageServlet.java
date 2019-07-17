c41ea569dc17eb2a26b3f092cef3e9230144dd88
AlexaBiloshytska <aleksandranecheporuk@gmail.com>
2019-07-17 22:09:09 +0300

    SearchCountryByCriteriaServlet modified;

--- src/main/java/com/alexa/worldservice/servlet/CountriesByLanguageServlet.java
+++ src/main/java/com/alexa/worldservice/servlet/CountriesByLanguageServlet.java
@@ -18,13 +18,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@JsonView(CountryData.class)
 @WebServlet(urlPatterns = "/api/v1/language")
 public class CountriesByLanguageServlet extends HttpServlet {
     private final Logger logger = LoggerFactory.getLogger(getClass());
-    private static final ObjectMapper mapper = ServiceLocator.get(ObjectMapper.class);
-    private CountryService countryService = ServiceLocator.get(CountryService.class);
-
+    private final ObjectMapper mapper = ServiceLocator.get(ObjectMapper.class);
+    private final CountryService countryService = ServiceLocator.get(CountryService.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -42,10 +40,6 @@ public class CountriesByLanguageServlet extends HttpServlet {
         } else {
             response.setStatus(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
         }
-
-        logger.info("Finished getting countries by language  {} ", language, startTime - System.currentTimeMillis());
+        logger.info("Finished getting countries by language {},{} ", language, startTime - System.currentTimeMillis());
     }
 }
-
-
-
