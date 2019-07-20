4adc09ece5bda3e9c58ae69c6c44f4b60fb49792
AlexaBiloshytska <aleksandranecheporuk@gmail.com>
2019-07-20 14:11:04 +0300

    SearchCountryByCriteria modified;

--- src/main/java/com/alexa/worldservice/servlet/SearchCountryByCriteriaServlet.java
+++ src/main/java/com/alexa/worldservice/servlet/SearchCountryByCriteriaServlet.java
@@ -19,7 +19,7 @@ import java.util.List;
 @WebServlet(urlPatterns = "/api/v1/search/country")
 public class SearchCountryByCriteriaServlet extends HttpServlet {
     private final Logger logger = LoggerFactory.getLogger(getClass());
-    private static final Integer LIMIT = 5;
+    private final Integer LIMIT = 5;
     private final XmlMapper xmlMapper = ServiceLocator.get(XmlMapper.class);
     private final CountryService countryService = ServiceLocator.get(CountryService.class);
 
@@ -39,10 +39,10 @@ public class SearchCountryByCriteriaServlet extends HttpServlet {
 
         String acceptType = request.getHeaders("Accept").nextElement();
 
-        logger.info("Getting country with parameters: {},{},{},{},{}", name, continent, population, limit, page);
+        logger.info("Getting country with parameters: {},{},{},{},{}", name, continent, population, page, limit);
 
         if (acceptType.contains(MimeType.APPLICATION_XML.getValue())) {
-            List<Country> country = countryService.getCountriesByCriteria(name, continent, population, limit, page);
+            List<Country> country = countryService.getCountriesByCriteria(name, continent, population, page, limit);
             String xml = xmlMapper.writerWithView(Views.CountryData.class).writeValueAsString(country);
             response.setContentType(MimeType.APPLICATION_XML.getValue());
             response.setStatus(HttpServletResponse.SC_OK);
